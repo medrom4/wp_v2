b88406f6a43540c79a7a078f33a1ddbad22567bc
Ramanovich <medikroma@gmail.com>
2019-01-24 19:21:51 +0000

    footer-add-sidebar_category

--- wp-content/themes/saprrow/functions.php
+++ wp-content/themes/saprrow/functions.php
@@ -9,6 +9,10 @@
     		'name'          => 'Left Sidebar',
     		'id'            => "left_sidebar",
     		'description'   => 'Описание моего сайдбара',
+		    'before_widget' => '<div class="widget %2$s">',
+		    'after_widget'  => "</div>\n",
+		    'before_title'  => '<h5 class="widgettitle">',
+		    'after_title'   => "</h5>\n"
     	) );
     }
 
